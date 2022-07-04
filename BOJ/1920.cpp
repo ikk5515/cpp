df38439303243534c5c17f870384e6383b43e23c
ikk5515 <ikk5515@naver.com>
2022-07-04 15:20:24 +0900

    BOJ_수 찾기

---
+++ BOJ/1920.cpp
@@ -0,0 +1,35 @@
+//BOJ_1920_수_찾기
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N, M;
+    int number;
+
+    cin >> N;           // 처음 입력받을 N개의 수를 입력
+    int arr[N];
+
+    for (int i = 0; i < N; i++) {
+        cin >> number;      // N만큼 입력받은 수를 arr배열에 넣어줌
+        arr[i] = number;
+    }
+    sort(arr, arr + N
+    );      // arr배열을 정렬해줌
+
+    cin >> M;
+    for (int i = 0; i < M; i++) {
+        cin >> number;
+        if (binary_search(arr, arr+N, number)) {
+            cout << 1 << "\n";
+        }
+        else {
+            cout << 0 << "\n";
+        }
+    }
+    return 0;
+}
