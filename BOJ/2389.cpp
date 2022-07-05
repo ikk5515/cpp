3aede60cee2bd051c3062e53fe7469424d3f3603
ikk5515 <ikk5515@naver.com>
2022-07-06 00:04:30 +0900

    BOJ_설탕 배달

--- BOJ/2389.cpp
+++ BOJ/2389.cpp
@@ -1,23 +1,36 @@
-//BOJ_2389_설탕_배달
+// BOJ_2839_설탕_배달
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
     int N = 0;
     int count = 0;
     cin >> N;
 
-    while (true) {
-        if (N % 5 == 0) {
-            N -= 5;
-            count++;
-        } else if (N % 3 == 0) {
-            N -= 3;
-            count++;
-        } else {
-            N = 0;
+    while (1)
+    {
+        if (N >= 3) {
+            if (N % 5 == 0) {
+                N -= 5;
+                count++;
+            }
+            else if (N % 3 == 0) {
+                N -= 3;
+                count++;
+            } else if (N > 5) {
+                N -= 5;
+                count++;
+            }
+        }
+        if (count == 0) {
+            count = -1;
+            break;
+        } else if (N == 0) {
+            break;
+        } else if ( N < 3 && N % 5 != 0 && N % 3 != 0) {
             count = -1;
             break;
         }
