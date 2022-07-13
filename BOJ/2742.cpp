a85919110d97db383cb7ad48588f6d2c277a5b9a
ikk5515 <ikk5515@naver.com>
2022-07-14 04:36:55 +0900

    BOJ_기찍 N

---
+++ BOJ/2742.cpp
@@ -0,0 +1,15 @@
+//BOJ_2742_기찍_N
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+    int n;
+    cin >> n;
+
+    for (int i = n; i > 0; i--) {
+        cout << i << "\n";
+    }
+    return 0;
+}
