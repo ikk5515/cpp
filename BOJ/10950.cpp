c5eb301180fe56369914dd687c17fb9b5d695af7
ikk5515 <ikk5515@naver.com>
2022-07-14 04:35:05 +0900

    BOJ_A+B-3

---
+++ BOJ/10950.cpp
@@ -0,0 +1,14 @@
+//BOJ_10950_A+B-3
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n ; i++) {
+        int a, b;
+        cin >> a >> b;
+        cout << a + b << "\n";
+    }
+    return 0;
+}
