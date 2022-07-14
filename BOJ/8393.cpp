7c22f654df2d67b0e8acdcc7b2ef6097f087a298
ikk5515 <ikk5515@naver.com>
2022-07-14 21:41:33 +0900

    BOJ_합

---
+++ BOJ/8393.cpp
@@ -0,0 +1,13 @@
+//BOJ_8393_합
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, sum = 0;
+    cin >> n;
+    for (int i = 1; i <= n ; i++) {
+        sum += i;
+    }
+    cout << sum << "\n";
+    return 0;
+}
