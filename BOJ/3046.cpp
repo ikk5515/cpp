cd810baa5721e009da522c9e9c2c70e096413f52
ikk5515 <ikk5515@naver.com>
2022-07-26 22:17:38 +0900

    BOJ_R2

---
+++ BOJ/3046.cpp
@@ -0,0 +1,12 @@
+//BOJ_3046_R2
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int R1, S;
+    cin >> R1 >> S;
+
+    cout << S * 2 - R1;
+    return 0;
+}
