efe5f30ec96a9492c164f72e1cd2f80f14b7262d
ikk5515 <ikk5515@naver.com>
2022-08-08 21:47:11 +0900

    층간소음

---
+++ test/18.cpp
@@ -0,0 +1,25 @@
+//층간소음
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n, m;
+    int arr[101] = { 0, };
+    int ans = 0, cnt = 0;
+
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > m) {
+            cnt++;
+        } else {
+            cnt = 0;
+        }
+        ans = max(ans, cnt);
+    }
+    cout << ans << "\n";
+    return 0;
+}
