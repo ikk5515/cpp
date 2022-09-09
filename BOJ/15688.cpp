4a4b54c9bb68ae74043f62a7b1dc8f1bc017f293
ikk5515 <ikk5515@naver.com>
2022-09-10 03:09:57 +0900

    BOJ_수 정렬하기5

---
+++ BOJ/15688.cpp
@@ -0,0 +1,22 @@
+//BOJ_15688_수_정렬하기_5
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int N;
+    int arr[1000001] = { 0, };
+
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+
+    sort (arr, arr+N);
+
+    for (int i = 0; i < N; i++) {
+        cout << arr[i] << "\n";
+    }
+    return 0;
+}
