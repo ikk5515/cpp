73f558702e7b35e3e227bcdfbf4f68f84bbc4afc
ikk5515 <ikk5515@naver.com>
2022-12-24 02:05:05 +0900

    BOJ_10172_개

---
+++ BOJ/10172.cpp
@@ -0,0 +1,12 @@
+//BOJ_10172_개
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "|\\" << "_/|\n";
+    cout << "|q p|   /}\n";
+    cout << "( 0 )\"\"\"\\\n";
+    cout << "|\"^\"`    |\n";
+    cout << "||_/=\\\\__|";
+    return 0;
+}
