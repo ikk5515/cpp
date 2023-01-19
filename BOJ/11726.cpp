a39f8693119caae36fa0cc13814131ebc90e721e
ikk5515 <ikk5515@naver.com>
2023-01-19 22:59:19 +0900

    BOJ_11726_2xn 타일링

---
+++ BOJ/11726.cpp
@@ -0,0 +1,16 @@
+// BOJ_11726_2xn 타일링
+#include <iostream>
+using namespace std;
+
+int dp[10001];
+int main() {
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    
+    int n; cin >> n;
+    dp[1] = 1; dp[2] = 2;
+    for (int i = 3; i <= n; i++) {
+        dp[i] = (dp[i-1] + dp[i-2])%10007;
+    }
+    cout << dp[n] << "\n";
+    return 0;
+}
