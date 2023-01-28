4c83df9315b45d97b35b8f719061523d4b58e288
ikk5515 <ikk5515@naver.com>
2023-01-29 01:49:45 +0900

    BOJ_15736_청기 백기

---
+++ BOJ/15736.cpp
@@ -0,0 +1,15 @@
+// BOJ_15736_청기 백기
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+    int n; cin >> n;
+
+    long long ans = 0;
+    ans = sqrt(n);
+    cout << ans;
+    return 0;
+}
