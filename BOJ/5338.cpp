6d5d495a7fc47fa4b65a45c1e6bc37a775204985
ikk5515 <ikk5515@naver.com>
2023-02-02 20:54:35 +0900

    BOJ_5338_마이크로소프트 로고

---
+++ BOJ/5338.cpp
@@ -0,0 +1,14 @@
+// BOJ_5338_마이크로소프트 로고
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+    cout << "       _.-;;-._\n";
+    cout << "'-..-'|   ||   |\n";
+    cout << "'-..-'|_.-;;-._|\n";
+    cout << "'-..-'|   ||   |\n";
+    cout << "'-..-'|_.-''-._|\n";
+    return 0;
+}
