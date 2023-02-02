3f998f1adaae4b1fffd7cec4bb4599796e42bc89
ikk5515 <ikk5515@naver.com>
2023-02-03 01:34:30 +0900

    BOJ_15733_나는 누구인가

---
+++ BOJ/15733.cpp
@@ -0,0 +1,9 @@
+// BOJ_15733_나는 누구인가
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    cout << "I'm Sexy";
+    return 0;
+}
