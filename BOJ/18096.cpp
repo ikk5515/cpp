348ea8b98332d2f5e95e328817eec2e2791d79c7
ikk5515 <ikk5515@naver.com>
2023-02-03 01:49:48 +0900

    BOJ_18096_Арифметическая магия

---
+++ BOJ/18096.cpp
@@ -0,0 +1,11 @@
+// BOJ_18096_Арифметическая магия
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+    int n; cin >> n;
+    cout << 1;
+    return 0;
+}
